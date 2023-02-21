5424774ee8e6cc68a13d16519c964b6f221eb5c7
timmoth <t.jones@timmoth.com>
2023-02-21 17:11:05 +0000

    initial commit

---
+++ blazor-periodic-timers/Pages/Index.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace blazor_periodic_timers.Pages
+{
+    public class IndexBase : ComponentBase, IDisposable
+    {
+        public static readonly string[] Greetings = new string[] {
+           "Hello",
+           "Hi",
+           "Hey there",
+           "Good morning",
+           "Good afternoon",
+           "Good evening",
+           "Howdy",
+           "What's up",
+           "Yo",
+           "Greetings",
+           "Salutations",
+           "Nice to see you",
+           "How are you doing",
+           "How's it going",
+           "What's new",
+           "Long time no see",
+           "Pleasure to meet you",
+           "Hey, how's everything",
+           "Hi there",
+           "Lovely day, isn't it"
+        };
+
+        public int GreetingIndex { get; private set; }
+
+        private readonly CancellationTokenSource _timerCancellationToken = new();
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+                while (!_timerCancellationToken.IsCancellationRequested && await timer.WaitForNextTickAsync())
+                {
+                    GreetingIndex = (GreetingIndex + 1) % Greetings.Length;
+                    await InvokeAsync(StateHasChanged);
+                }
+                // Make sure base.OnAfterRenderAsync is not called after the component is disposed
+                return;
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+        public void Dispose()
+        {
+            _timerCancellationToken.Cancel();
+            _timerCancellationToken.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ blazor-periodic-timers/Pages/TheOldWay.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace blazor_periodic_timers.Pages
+{
+    public class TheOldWayBase : ComponentBase, IDisposable
+    {
+        public static readonly string[] Greetings = new string[] {
+           "Hello",
+           "Hi",
+           "Hey there",
+           "Good morning",
+           "Good afternoon",
+           "Good evening",
+           "Howdy",
+           "What's up",
+           "Yo",
+           "Greetings",
+           "Salutations",
+           "Nice to see you",
+           "How are you doing",
+           "How's it going",
+           "What's new",
+           "Long time no see",
+           "Pleasure to meet you",
+           "Hey, how's everything",
+           "Hi there",
+           "Lovely day, isn't it"
+        };
+        public int GreetingIndex { get; private set; }
+
+        private static readonly System.Timers.Timer _timer = new System.Timers.Timer(1000);
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _timer.Elapsed += Timer_Elapsed;
+                _timer.Enabled = true;
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        private async void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            GreetingIndex = (GreetingIndex + 1) % Greetings.Length;
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            _timer.Stop();
+            _timer.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
